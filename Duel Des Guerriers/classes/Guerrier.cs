f5ce37ac4aafd5c4716ba67c5f4be5d8831cab36
kevin2759 <soulhol.kevin27@gmail.com>
2024-12-26 11:10:30 +0100

    Ajoutez des fichiers projet.

---
+++ Duel Des Guerriers/classes/Guerrier.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duel_Des_Guerriers.classes
+{
+    internal class Guerrier
+    {
+
+        //attributs privé
+        private string nom;
+        private int pointsDeVie;
+        private int nbDesAttaque;
+
+        //constructeur
+        public Guerrier(string nom, int pointsDeVie, int nbDesAttaque)
+        {
+            this.nom = nom;
+            this.pointsDeVie = pointsDeVie;
+            this.nbDesAttaque = nbDesAttaque;
+
+        }
+        
+        // Getters
+        public string Nom()
+        {
+            return this.nom;
+        }
+
+        public int PointsDeVie()
+        {
+            return this.pointsDeVie;
+        }
+
+        public int NbDesAttaque()
+        {
+            return this.nbDesAttaque;
+        }
+
+        //Setters
+    }
+
+
+       
+}
